4f1978ff62329ed0ce10d1946212f9ea063f6084
Ioannis Nikitopoulos <107237745+giannis198@users.noreply.github.com>
2023-05-27 21:23:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
